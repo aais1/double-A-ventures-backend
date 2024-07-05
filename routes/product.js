a079464075a5271352569fad5cf64748797138dc
AliAais <aaisali228@gmail.com>
2024-07-05 04:35:09 -0700

    adding review route and controllers

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,13 @@
 const router=require('express').Router();
 const { newProductController, getProductsController, getProductController, updateProductController, deleteProductController } = require('../controllers/product')
+const reviewRouter=require('./review')
 
 router.post('/',newProductController);
 router.get('/',getProductsController);
 router.get('/:id',getProductController);
 router.put('/:id',updateProductController);
 router.delete('/:id',deleteProductController);
+router.use('/:id/review',reviewRouter)
 
 
 module.exports=router;
